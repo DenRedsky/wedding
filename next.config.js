cb74d17d4872fd62bfd7ed5124498aa66a56d076
Denis <denredsky@mail.ru>
2021-05-06 19:04:53 +0300

    fix path

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 module.exports = {
   distDir: 'build',
   assetPrefix:  isProduction? 'https://denredsky.github.io/wedding' : '',
-  basePath: isProduction ? 'https://denredsky.github.io/wedding' : '',
+  basePath: isProduction ? '/wedding' : '',
   future: {
     webpack5: true
   }
