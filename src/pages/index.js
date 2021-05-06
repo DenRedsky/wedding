f928b2807636fa325c40fed3837d432cba79c7e2
Denis <denredsky@mail.ru>
2021-05-06 20:08:27 +0300

    preload webp

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,12 +11,22 @@ export default function Main() {
   return (
     <>
       <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="preload"
+          href="https://denredsky.github.io/wedding/couple.webp"
+          as="image"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Alegreya+SC:wght@400;500&family=Amatic+SC:wght@700&display=swap"
           rel="stylesheet"
         />
-        <title>Wedding</title>
+        <title>
+          Свадьба Дениса и Ирины
+        </title>
       </Head>
       <main>
         <Header />
