90decc4fb1511c567a03c939c2d4668cf4e04201
DenRedsky <denredsky@mail.ru>
2021-05-07 13:53:09 +0000

    Deploying to gh-pages from  @ 1fe281d6c6e6020eace6a7c93b721b66ad169621 🚀

---
+++ _next/static/YqCge-7nFmrsGtytZpzx7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a3d567664cbaf410ae6c.css","static/chunks/pages/index-3228007f5ab5be6784f6.js"],"/_error":["static/chunks/pages/_error-7c76a9895163e492069d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/mAOB0Fqy_uIuJhNiAkpHq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a3d567664cbaf410ae6c.css","static/chunks/pages/index-d4efbdbde44e68fbf511.js"],"/_error":["static/chunks/pages/_error-7c76a9895163e492069d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
