bff0a40f45f4ee29da5ef58ef19e97bdedba3cb2
Denis <denredsky@mail.ru>
2021-05-07 11:15:22 +0300

    no message

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+const withPWA = require('next-pwa');
+
 const isProduction = process.env.NODE_ENV === 'production';
 
-module.exports = {
+module.exports = withPWA({
   assetPrefix:  isProduction? 'https://denredsky.github.io/wedding' : '',
   future: {
     webpack5: true
+  },
+  pwa: {
+    dest: 'public'
   }
-}
+});
