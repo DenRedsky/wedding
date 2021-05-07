74bac3a4fdc41a171d3d47faf4eaeb5a8f050004
DenRedsky <denredsky@mail.ru>
2021-05-07 09:35:47 +0000

    Deploying to gh-pages from  @ f2dbcf7b44ef61fc1a83fef0afa16aa7ee6cc2b5 🚀

--- _next/static/NTpFm_Ux4wZxu13ROjcdu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/df0748958be81bb92f6c.css","static/chunks/pages/index-f0b8edaa0b33b765673b.js"],"/_error":["static/chunks/pages/_error-7c76a9895163e492069d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
